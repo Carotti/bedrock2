613a0082de2e453eee30772247ebb8d109ea9d91
Thomas Carotti <thomas@carotti.co.uk>
2019-02-28 23:17:12 -0500

    Updating weakest precondition

--- bedrock2/src/WeakestPrecondition.v
+++ bedrock2/src/WeakestPrecondition.v
@@ -47,74 +47,74 @@ Section WeakestPrecondition.
   End WithF.
 
   Section WithFunctions.
-    Context (call : funname -> trace -> mem -> list word -> (trace -> mem -> list word -> Prop) -> Prop).
+    Context (call : funname -> trace -> mem -> list word -> metrics -> (trace -> mem -> list word -> metrics -> Prop) -> Prop).
     Definition dexpr m l e v := expr m l e (eq v).
     Definition dexprs m l es vs := list_map (expr m l) es (eq vs).
-    Definition cmd_body (rec:_->_->_->_->_->Prop) (c : cmd) (t : trace) (m : mem) (l : locals)
-             (post : trace -> mem -> locals -> Prop) : Prop :=
+    Definition cmd_body (rec:_->_->_->_->_->_->Prop) (c : cmd) (t : trace) (m : mem) (l : locals) (mc : metrics)
+             (post : trace -> mem -> locals -> metrics -> Prop) : Prop :=
       (* give value of each pure expression when stating its subproof *)
       match c with
-      | cmd.skip => post t m l
+      | cmd.skip => post t m l mc
       | cmd.set x ev =>
         bind_ex v <- dexpr m l ev;
         dlet! l := map.put l x v in
-        post t m l
+        post t m l mc
       | cmd.unset x =>
         dlet! l := map.remove l x in
-        post t m l
+        post t m l mc
       | cmd.store sz ea ev =>
         bind_ex a <- dexpr m l ea;
         bind_ex v <- dexpr m l ev;
         store sz m a v (fun m =>
-        post t m l)
+        post t m l mc)
       | cmd.cond br ct cf =>
         bind_ex v <- dexpr m l br;
-        (word.unsigned v <> 0%Z -> rec ct t m l post) /\
-        (word.unsigned v = 0%Z -> rec cf t m l post)
+        (word.unsigned v <> 0%Z -> rec ct t m l mc post) /\
+        (word.unsigned v = 0%Z -> rec cf t m l mc post)
       | cmd.seq c1 c2 =>
-        rec c1 t m l (fun t m l => rec c2 t m l post)
+        rec c1 t m l mc (fun t m l mc => rec c2 t m l mc post)
       | cmd.while e c =>
         exists measure (lt:measure->measure->Prop) (inv:measure->trace->mem->locals->Prop),
         Coq.Init.Wf.well_founded lt /\
         (exists v, inv v t m l) /\
         (forall v t m l, inv v t m l ->
           bind_ex b <- dexpr m l e;
-          (word.unsigned b <> 0%Z -> rec c t m l (fun t' m l =>
+          (word.unsigned b <> 0%Z -> rec c t m l mc (fun t' m l mc =>
             exists v', inv v' t' m l /\ lt v' v)) /\
-          (word.unsigned b = 0%Z -> post t m l))
+          (word.unsigned b = 0%Z -> post t m l mc))
       | cmd.call binds fname arges =>
         bind_ex args <- dexprs m l arges;
-        call fname t m args (fun t m rets =>
+        call fname t m args mc (fun t m rets mc =>
           bind_ex_Some l <- map.putmany_of_list binds rets l;
-          post t m l)
+          post t m l mc)
       | cmd.interact binds action arges =>
         bind_ex args <- dexprs m l arges;
         ext_spec t m action args (let old_mem := m in fun m rets =>
           bind_ex_Some l <- map.putmany_of_list binds rets l;
-          post (cons ((old_mem, action, args), (m, rets)) t) m l)
+          post (cons ((old_mem, action, args), (m, rets)) t) m l mc)
       end.
     Fixpoint cmd c := cmd_body cmd c.
   End WithFunctions.
 
-  Definition func call '(innames, outnames, c) (t : trace) (m : mem) (args : list word) (post : trace -> mem -> list word -> Prop) :=
+  Definition func call '(innames, outnames, c) (t : trace) (m : mem) (mc : metrics) (args : list word) (post : trace -> mem -> list word -> metrics -> Prop) :=
       bind_ex_Some l <- map.of_list innames args;
-      cmd call c t m l (fun t m l =>
+      cmd call c t m l mc (fun t m l mc =>
         list_map (get l) outnames (fun rets =>
-        post t m rets)).
+        post t m rets mc)).
 
   Definition call_body rec (functions : list (funname * (list varname * list varname * cmd.cmd)))
-                (fname : funname) (t : trace) (m : mem) (args : list word)
-                (post : trace -> mem -> list word -> Prop) : Prop :=
+                (fname : funname) (t : trace) (m : mem) (args : list word) (mc : metrics)
+                (post : trace -> mem -> list word -> metrics -> Prop) : Prop :=
     match functions with
     | nil => False
     | cons (f, decl) functions =>
       if funname_eqb f fname
-      then func (rec functions) decl t m args post
-      else rec functions fname t m args post
+      then func (rec functions) decl t m mc args post
+      else rec functions fname t m args mc post
     end.
   Fixpoint call functions := call_body call functions.
 
-  Definition program funcs main t m l post : Prop := cmd (call funcs) main t m l post.
+  Definition program funcs main t m l mc post : Prop := cmd (call funcs) main t m l mc post.
 End WeakestPrecondition.
 
 Ltac unfold1_cmd e :=
